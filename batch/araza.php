5cc57690883b5cf644c7dd1f7aa5f38c912e7180
Ali Raza <araza@globeandmail.com>
2017-02-03 16:02:45 -0500

    Site added for Lab1

---
+++ batch/araza.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ali Raza</title>
+</head>
+<body>
+
+<h1>Ali Raza</h1>
+<h2>Role: Instructor</h2>
+<br />
+<p>Experienced DevOps Engineer understands the melding of operations and development and capable of transforming the requirements of various agile teams to operations. Has experience with all aspects of DevOps process from system management to delivery of application in Windows, Mac and Linux systems. Experience and education combined with positive attitude can allow to help a progressive organization meets and exceeds its growing and constant IT challenges.</p>
+
+</body>
+</html>
---
+++ batch/list.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<br />
+<h2>Cloud Computing 101 - Batch</h2>
+<br />
+<br />
+
+<table>
+<tr>
+
+<td width="25%">
+	<ul>
+		<li><a href="araza.php" target="iframe_a">Ali Raza</a></li>
+		<li>* * * * * * * *</li>
+		<!-- Add your names below -->
+	</ul>
+</td>
+
+<td width="65%">
+	<iframe height="300px" width="90%" src="demo_iframe.htm" name="iframe_a"></iframe>
+</td>
+
+</tr>
+</table>
+	
+</body>
+</html>
